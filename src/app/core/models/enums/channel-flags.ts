9b10240ee4a5a0bb679ab85fa7c472f0f562a005
Michal Halabica <m.halabica@gmail.com>
2022-05-16 20:34:22 +0200

    Implemented option to deactivate all autoreplies in the channel

--- src/app/core/models/enums/channel-flags.ts
+++ src/app/core/models/enums/channel-flags.ts
@@ -1,19 +1,24 @@
 export enum ChannelFlags {
     StatsHidden = 1,
-    CommandsDisabled = 2
+    CommandsDisabled = 2,
+    Deleted = 4,
+    AutoReplyDeactivated = 8
 }
 
 export enum ChannelSettingsFlags {
     StatsHidden = ChannelFlags.StatsHidden,
-    CommandsDisabled = ChannelFlags.CommandsDisabled
+    CommandsDisabled = ChannelFlags.CommandsDisabled,
+    AutoReplyDeactivated = ChannelFlags.AutoReplyDeactivated
 }
 
 export enum ChannelFlagTexts {
     StatsHidden = 'Skryté statistiky',
-    CommandsDisabled = 'Vypnuté příkazy'
+    CommandsDisabled = 'Vypnuté příkazy',
+    AutoReplyDeactivated = 'Automatické odpovědi vypnuty'
 }
 
 export const ChannelFlagMapping = [
     { source: ChannelSettingsFlags.StatsHidden, destination: ChannelFlags.StatsHidden },
-    { source: ChannelSettingsFlags.CommandsDisabled, destination: ChannelFlags.CommandsDisabled }
+    { source: ChannelSettingsFlags.CommandsDisabled, destination: ChannelFlags.CommandsDisabled },
+    { source: ChannelSettingsFlags.AutoReplyDeactivated, destination: ChannelFlags.AutoReplyDeactivated }
 ];
