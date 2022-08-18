7e35ca9df2e6e09f4c12575ef8134df4b195538f
Michal Halabi <m.halabica@gmail.com>
2022-08-18 23:58:34 +0200

    Event logs

--- src/app/administration/internal/statistics/statistics.component.ts
+++ src/app/administration/internal/statistics/statistics.component.ts
@@ -3,6 +3,7 @@ import { ObservableDict, ObservableList } from './../../../core/models/common';
 import { StatisticsService } from './../../../core/services/statistics.service';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { StatisticItem } from 'src/app/core/models/statistics';
+import { SystemService } from 'src/app/core/services/system.service';
 
 @Component({
     selector: 'app-statistics',
@@ -13,8 +14,12 @@ export class StatisticsComponent implements OnInit, OnDestroy {
 
     statItemsQuery: ObservableList<StatisticItem>;
     dictQuery: ObservableDict<string, number>;
+    listQuery: ObservableList<string>;
 
-    constructor(private statisticsService: StatisticsService) { }
+    constructor(
+        private statisticsService: StatisticsService,
+        private systemService: SystemService
+    ) { }
 
     get isDb(): boolean { return this.type === 'db'; }
     get isDbCache(): boolean { return this.type === 'db/cache'; }
@@ -28,6 +33,7 @@ export class StatisticsComponent implements OnInit, OnDestroy {
     get isApiRequestsByDate(): boolean { return this.type === 'api/date'; }
     get isApiRequestsByEndpoint(): boolean { return this.type === 'api/endpoint'; }
     get isApiRequestsByStatusCode(): boolean { return this.type === 'api/status'; }
+    get isEventLog(): boolean { return this.type === 'eventLog'; }
 
     get isStatLike(): boolean {
         return this.isTextCommands || this.isInteractions || this.isJobs || this.isApiRequestsByEndpoint;
@@ -41,36 +47,42 @@ export class StatisticsComponent implements OnInit, OnDestroy {
         return this.isDb || this.isDbCache || this.isAuditLogByType || this.isUnverifyLogByType || this.isApiRequestsByStatusCode;
     }
 
+    get isList(): boolean {
+        return this.isEventLog;
+    }
+
     get header(): string {
         if (this.isDb) { return 'Statistika databáze'; }
-        else if (this.isDbCache) { return 'Statistika DB cache'; }
-        else if (this.isAuditLogByType) { return 'Statistika audit logu (podle typu)'; }
-        else if (this.isAuditLogByDate) { return 'Statistika audit logu (po měsících)'; }
-        else if (this.isUnverifyLogByType) { return 'Statistika unverify logu (podle typu)'; }
-        else if (this.isUnverifyLogByDate) { return 'Statistika unverify logu (po měsících)'; }
-        else if (this.isTextCommands) { return 'Statistika textových příkazů'; }
-        else if (this.isInteractions) { return 'Statistika interaktivních příkazů'; }
-        else if (this.isJobs) { return 'Statistika naplánovaných úloh'; }
-        else if (this.isApiRequestsByDate) { return 'Statistika API požadavků (po měsících)'; }
-        else if (this.isApiRequestsByEndpoint) { return 'Statistika API požadavků (po metodách)'; }
-        else if (this.isApiRequestsByStatusCode) { return 'Statistika API požadavků (podle výsledku)'; }
+        if (this.isDbCache) { return 'Statistika DB cache'; }
+        if (this.isAuditLogByType) { return 'Statistika audit logu (podle typu)'; }
+        if (this.isAuditLogByDate) { return 'Statistika audit logu (po měsících)'; }
+        if (this.isUnverifyLogByType) { return 'Statistika unverify logu (podle typu)'; }
+        if (this.isUnverifyLogByDate) { return 'Statistika unverify logu (po měsících)'; }
+        if (this.isTextCommands) { return 'Statistika textových příkazů'; }
+        if (this.isInteractions) { return 'Statistika interaktivních příkazů'; }
+        if (this.isJobs) { return 'Statistika naplánovaných úloh'; }
+        if (this.isApiRequestsByDate) { return 'Statistika API požadavků (po měsících)'; }
+        if (this.isApiRequestsByEndpoint) { return 'Statistika API požadavků (po metodách)'; }
+        if (this.isApiRequestsByStatusCode) { return 'Statistika API požadavků (podle výsledku)'; }
+        if (this.isEventLog) { return 'Log událostí'; }
 
         return '';
     }
 
     ngOnInit(): void {
         if (this.isDb) { this.dictQuery = this.statisticsService.getDbStatus(); }
-        else if (this.isDbCache) { this.dictQuery = this.statisticsService.getDbCacheStatus(); }
-        else if (this.isAuditLogByType) { this.dictQuery = this.statisticsService.getAuditLogsStatisticsByType(); }
-        else if (this.isAuditLogByDate) { this.dictQuery = this.statisticsService.getAuditLogsStatisticsByDate(); }
-        else if (this.isUnverifyLogByType) { this.dictQuery = this.statisticsService.getUnverifyLogsStatisticsByOperation(); }
-        else if (this.isUnverifyLogByDate) { this.dictQuery = this.statisticsService.getUnverifyLogsStatisticsByDate(); }
-        else if (this.isTextCommands) { this.statItemsQuery = this.statisticsService.getCommandsStatistics(); }
-        else if (this.isInteractions) { this.statItemsQuery = this.statisticsService.getInteractionsStatus(); }
-        else if (this.isJobs) { this.statItemsQuery = this.statisticsService.getJobsStatistics(); }
-        else if (this.isApiRequestsByDate) { this.dictQuery = this.statisticsService.getApiRequestsByDate(); }
-        else if (this.isApiRequestsByEndpoint) { this.statItemsQuery = this.statisticsService.getApiRequestsByEndpoint(); }
-        else if (this.isApiRequestsByStatusCode) { this.dictQuery = this.statisticsService.getApiRequestsByStatusCode(); }
+        if (this.isDbCache) { this.dictQuery = this.statisticsService.getDbCacheStatus(); }
+        if (this.isAuditLogByType) { this.dictQuery = this.statisticsService.getAuditLogsStatisticsByType(); }
+        if (this.isAuditLogByDate) { this.dictQuery = this.statisticsService.getAuditLogsStatisticsByDate(); }
+        if (this.isUnverifyLogByType) { this.dictQuery = this.statisticsService.getUnverifyLogsStatisticsByOperation(); }
+        if (this.isUnverifyLogByDate) { this.dictQuery = this.statisticsService.getUnverifyLogsStatisticsByDate(); }
+        if (this.isTextCommands) { this.statItemsQuery = this.statisticsService.getCommandsStatistics(); }
+        if (this.isInteractions) { this.statItemsQuery = this.statisticsService.getInteractionsStatus(); }
+        if (this.isJobs) { this.statItemsQuery = this.statisticsService.getJobsStatistics(); }
+        if (this.isApiRequestsByDate) { this.dictQuery = this.statisticsService.getApiRequestsByDate(); }
+        if (this.isApiRequestsByEndpoint) { this.statItemsQuery = this.statisticsService.getApiRequestsByEndpoint(); }
+        if (this.isApiRequestsByStatusCode) { this.dictQuery = this.statisticsService.getApiRequestsByStatusCode(); }
+        if (this.isEventLog) { this.listQuery = this.systemService.getEventLog(); }
 
         if (this.dictQuery) {
             this.dictQuery = this.dictQuery.pipe(map(data => data.reverse()));
@@ -80,5 +92,6 @@ export class StatisticsComponent implements OnInit, OnDestroy {
     ngOnDestroy(): void {
         this.statItemsQuery = null;
         this.dictQuery = null;
+        this.listQuery = null;
     }
 }
--- src/app/core/services/system.service.ts
+++ src/app/core/services/system.service.ts
@@ -1,9 +1,9 @@
+import { ObservableList } from './../models/common';
 import { catchError, map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { BaseService } from './base.service';
 import { DiagnosticsInfo } from '../models/system';
-import { environment } from 'src/environments/environment';
 import { HttpErrorResponse } from '@angular/common/http';
 import { QueryParam } from '../models/http';
 
@@ -14,7 +14,7 @@ export class SystemService {
     ) { }
 
     getDiagnostics(): Observable<DiagnosticsInfo> {
-        const url = `${environment.apiUrl}/system/diag`;
+        const url = this.base.createUrl('system/diag', null);
         const headers = this.base.getHttpHeaders();
 
         return this.base.http.get<DiagnosticsInfo>(url, { headers }).pipe(
@@ -24,12 +24,20 @@ export class SystemService {
     }
 
     setBotState(isActive: boolean): Observable<unknown> {
-        const parameters = new QueryParam('isActive', isActive).toString();
-        const url = `${environment.apiUrl}/system/status?${parameters}`;
+        const url = this.base.createUrl('system/status', [new QueryParam('isActive', isActive)]);
         const headers = this.base.getHttpHeaders();
 
         return this.base.http.put(url, null, { headers }).pipe(
             catchError((err: HttpErrorResponse) => this.base.catchError(err))
         );
+    }
+
+    getEventLog(): ObservableList<string> {
+        const url = this.base.createUrl('system/eventLog', null);
+        const headers = this.base.getHttpHeaders();
+
+        return this.base.http.get<string[]>(url, { headers }).pipe(
+            catchError((err: HttpErrorResponse) => this.base.catchError(err))
+        )
     }
 }
