4519be35d18da67c53edd744fb151ee0b9cbaeba
Michal Halabica <m.halabica@gmail.com>
2022-11-13 16:58:32 +0100

    Fixed name of container repository

--- deploy.ps1
+++ deploy.ps1
@@ -1,4 +1,4 @@
 git pull --rebase;
 git push -u origin;
-docker build -t ghcr.io/grillbot/grillbot-web .;
-docker push ghcr.io/grillbot/grillbot-web;
+docker build -t ghcr.io/grillbot/grillbot-private-web .;
+docker push ghcr.io/grillbot/grillbot-private-web;
